8c6d1f90dc202aaebade6f7996d6a6e3ecb1c6da
kelldezim <kelldezim@gmail.com>
2022-09-15 10:59:08 +0200

    Add project files.

---
+++ src/SlotManager.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlotManager.Api.Models;
+
+namespace SlotManager.Api.Controllers
+{
+    [ApiController]
+    [Route("reservations")]
+    
+    public class ReservationsController : ControllerBase
+    {
+        private int _id = 1;
+        private readonly List<Reservation> _reservations = new();
+        private readonly List<string> _parkingSlots = new()
+        {
+            "P1",
+            "P2",
+            "P3",
+            "P4",
+            "P5"
+        };
+
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+
+        [HttpPost]
+        public void Post(Reservation reservation)
+        {
+            if(_parkingSlots.All(x => x != reservation.ParkingSpotName))
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            reservation.Id = _id;
+            reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+            _id++;
+        }
+    }
+}
